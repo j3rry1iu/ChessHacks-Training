31aecfa00b417e3f19fa52ab6089660cce7222e1
Hamza <hamza.k.ammar@gmail.com>
2025-11-15 14:13:57 -0500

    Added board encoder

---
+++ src/board_encoder.py
@@ -0,0 +1,29 @@
+import chess
+import numpy as np
+
+def encode_board(board: chess.Board) -> np.ndarray:
+    array = np.zeros((12,8,8), dtype=np.int8)
+    piece_to_index = {
+        chess.PAWN: 0,
+        chess.KNIGHT: 1,
+        chess.BISHOP: 2,
+        chess.ROOK: 3,
+        chess.QUEEN: 4,
+        chess.KING: 5
+    }   
+    for square, piece in board.piece_map().items():
+        rank = chess.square_rank(square)
+        file = chess.square_file(square)
+        piece_type = piece.piece_type
+        color = piece.color
+        color_offset = 0 if color == chess.WHITE else 6
+        idx = piece_type - 1
+
+        array[ color_offset + idx, 7-rank, file] = 1
+    return array
+
+if __name__ == "__main__":
+    board = chess.Board()
+    print(board)
+    encoded = encode_board(board)
+    print(encoded)
